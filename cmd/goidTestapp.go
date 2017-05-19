fd32e494987d370723dce10277e6371792b73892
jakehl <jakelangford@outlook.com>
2017-05-19 17:08:56 +0100

    Implementing ToString

--- cmd/goidTestapp.go
+++ cmd/goidTestapp.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"encoding/hex"
+
 	"fmt"
 
 	"github.com/jakehl/goid"
 )
 
 func main() {
-	a := &goid.UUID{}
-	fmt.Println(a)
+	bytes := [16]byte{}
+	for i := 0; i < 16; i++ {
+		byteVals, _ := hex.DecodeString("ff")
+		bytes[i] = byteVals[0]
+	}
+
+	guid := &goid.UUID{Version: 4, Octets: bytes}
+	fmt.Println(guid.ToString())
+
 }
--- goid.go
+++ goid.go
@@ -2,10 +2,24 @@ package goid
 
 // UUID : Represents a UUID or GUID
 type UUID struct {
+	Version int
+	Octets  [16]byte
 }
 
 // ToString : Converts the desired guid into a string
 func (g *UUID) ToString() string {
 	// TODO Implement UUID to string
-	return "Not implemented"
+	var result string
+
+	var iterations = [5]int{7, 11, 15, 19, 31}
+
+	for _, oc := range g.Octets {
+		result += string(oc)
+	}
+
+	for _, it := range iterations {
+		result = result[:it] + "-" + result[it:]
+	}
+
+	return result
 }
